2c1c5fd036cb3d0b884b3c6c7264dd3168a0ad83
ReubenKabiti <reubenrk2@gmail.com>
2023-05-30 17:28:19 +0200

    feature: get key

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ def get_key_from_user():
     key = input("New user\nEnter API key (given to you when you create an account on pastebin): ")
     return key
 
-def init():
+def get_key():
+
+    key = None
 
     if not os.path.exists("api.key"):
         key = get_key_from_user()
         with open("api.key", "w") as file:
             file.write(key)
+    else:
+
+        with open("api.key", "r") as file:
+            key = file.read()
+
+    return key
+
+
+print(get_key())
 
